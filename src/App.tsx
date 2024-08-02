c1b15a19d5577463198084a7769006a66b31eae4
mehdi_moujahed <mehdi.moujahed@astrolab-agency.com>
2024-08-02 19:03:27 +0200

    integrating component in App file

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import FaceRecognition from "./components/Facerecognition";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <FaceRecognition />
+    </div>
+  );
+};
+
+export default App;
